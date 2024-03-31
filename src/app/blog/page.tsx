5dc9aa5823693b86b0128af67eb509efa70d3531
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-03-31 14:33:15 +0500

    initial commit

---
+++ src/app/blog/page.tsx
@@ -0,0 +1,5 @@
+function Blog() {
+  return <h1>Hello, this is my Blog Page</h1>;
+}
+
+export default Blog;
---
+++ src/app/contact_us/page.tsx
@@ -0,0 +1,5 @@
+function ContactUs() {
+  return <h1>Hello, this is Contact US Page</h1>;
+}
+
+export default ContactUs;
