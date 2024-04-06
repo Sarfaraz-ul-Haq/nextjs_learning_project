9f2d3084ad55ecf1df97e31151d6b2500e7e1c09
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-07 00:21:55 +0500

    edit footer text

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
   return (
     <footer className="fixed bottom-0 w-full bg-gray-400 text-center">
-      <p className="py-4">Your footer content here</p>
+      <p className="py-4">{`Hello, I'm a footer component`}</p>
     </footer>
   );
 }
