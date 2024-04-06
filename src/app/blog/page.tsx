f48441d867a7fc6c457a79468ca99741357382e9
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-04-07 01:57:46 +0500

    add padding

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,14 @@
+import BackButton from "@/components/back_button";
+
 function Blog() {
-  return <h1>Hello, this is my Blog Page</h1>;
+  return (
+    <div className="flex flex-col gap-3 py-3 px-5">
+      <div>Hello, this is my Blog Page</div>
+      <div>
+        <BackButton />
+      </div>
+    </div>
+  );
 }
 
 export default Blog;
