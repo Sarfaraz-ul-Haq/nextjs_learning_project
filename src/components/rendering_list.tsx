67d973a43b9f218a2471bf3bf052b227fe809762
Sarfaraz ul Haq <isarfaraz80@gmail.com>
2024-07-14 11:48:40 +0500

    rendering list practice

--- src/components/rendering_list.tsx
+++ src/components/rendering_list.tsx
@@ -1,5 +1,31 @@
-const fruits = ["mango", "guava", "peach", "lychee", "strawberries"];
+import React from "react";
 
-function RenderFruits() {
-  return <ul>{fruits.map((fruit, index) => fruit)}</ul>;
+const products = [
+  { title: "tea", id: 1 },
+  { title: "coffee", id: 2 },
+  { title: "dates", id: 3 },
+  { title: "milk", id: 4 },
+  { title: "cheese", id: 5 },
+  { title: "pizza", id: 6 },
+  { title: "porridge", id: 7 },
+  { title: "yogurt", id: 8 },
+];
+
+function RenderingList() {
+  return (
+    <div className="p-5">
+      RenderingList
+      <div>
+        {products.map((item) => {
+          return (
+            <div key={item.id} className="bg-green-100 mt-3">
+              Title: {item.title} | id: {item.id}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 }
+
+export default RenderingList;
